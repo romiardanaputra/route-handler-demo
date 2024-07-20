5a1ced1dca6bf342f79a6662cddab1c71c6c68a3
romiardanaputra <romiardana21@gmail.com>
2024-07-20 17:39:00 +0800

    docs(route): Create docs for get and post request handler

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,10 +1,21 @@
 import { headers } from "next/headers";
 import { comments } from "./data";
 
+/**
+ * A description of the entire function.
+ *
+ * @return {type} description of return value
+ */
 export async function GET() {
   return Response.json(comments);
 }
 
+/**
+ * Function to handle POST requests for adding new comments.
+ *
+ * @param {Request} request - The request object containing the comment data.
+ * @return {Response} The response object with the added comment and status.
+ */
 export async function POST(request: Request) {
   const body = await request.json();
   const newComment = {
